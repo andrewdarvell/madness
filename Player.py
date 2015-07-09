9f27ae3f23dafb43debcb83a7ce070c14bf42b93
darvell <andrew@darvell.ru>
2015-07-09 22:17:01 +0300

    Minimum for game

--- Player.py
+++ Player.py
@@ -13,21 +13,42 @@ class Player(sprite.Sprite):
     def __init__(self, x, y):
         sprite.Sprite.__init__(self)
         self.xvel = 0
+        self.yvel = 0
         self.startX = x
         self.startY = y
         self.image = Surface((WIDTH, HEIGHT))
-        self.image.fill = (Color(COLOR))
+        self.image.fill(Color(COLOR))
         self.rect = Rect(x, y, WIDTH, HEIGHT)
 
-    def update(self, left, right):
+    def update(self, left, right, up, down, platforms):
         if left:
             self.xvel = -MOVE_SPEED
         if right:
             self.xvel = MOVE_SPEED
-        if not(left or right):
+        if up:
+            self.yvel = -MOVE_SPEED
+        if down:
+            self.yvel = MOVE_SPEED
+
+        if not(left or right or up or down):
             self.xvel = 0
+            self.yvel = 0
+
+        self.rect.y += self.yvel
+        self.collide(0, self.yvel, platforms)
 
         self.rect.x += self.xvel
+        self.collide(self.xvel, 0, platforms)
 
-    def draw(self, screen):
-        screen.blit(self.image, (self.rect.x,self.rect.y))
+    def collide(self, xvel, yvel, platforms):
+        for p in platforms:
+            if sprite.collide_rect(self, p):
+                if xvel > 0:
+                    self.rect.right = p.rect.left
+                if xvel < 0:
+                    self.rect.left = p.rect.right
+                if yvel > 0:
+                    self.rect.bottom = p.rect.top
+                if yvel < 0:
+                    self.rect.top = p.rect.bottom
+
--- madness.py
+++ madness.py
@@ -2,7 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import pygame
+
 from pygame import *
+from Blocks import Platform
+from Player import Player
+
 
 WIN_WIDTH = 800
 WIN_HEIGHT = 640
@@ -14,26 +18,53 @@ PLATFORM_HEIGHT = 32
 PLATFORM_COLOR = "#FF6262"
 
 level = [
-       "-------------------------",
-       "-                       -",
-       "-                       -",
-       "-                       -",
-       "-            --         -",
-       "-                       -",
-       "--                      -",
-       "-                       -",
-       "-                   --- -",
-       "-                       -",
-       "-                       -",
-       "-      ---              -",
-       "-                       -",
-       "-   -----------         -",
-       "-                       -",
-       "-                -      -",
-       "-                   --  -",
-       "-                       -",
-       "-                       -",
-       "-------------------------"]
+       "----------------------------------",
+       "-                                -",
+       "-                       --       -",
+       "-                                -",
+       "-            --                  -",
+       "-                                -",
+       "--                               -",
+       "-                                -",
+       "-                   ----     --- -",
+       "-                                -",
+       "--                               -",
+       "-                                -",
+       "-                            --- -",
+       "-                                -",
+       "-                                -",
+       "-      ---                       -",
+       "-                                -",
+       "-   -------         ----         -",
+       "-                                -",
+       "-                         -      -",
+       "-                            --  -",
+       "-                                -",
+       "-                                -",
+       "----------------------------------"]
+
+class Camera(object):
+    def __init__(self, camera_func, width, height):
+        self.camera_func = camera_func
+        self.state = Rect(0, 0, width, height)
+
+    def apply(self, target):
+        return target.rect.move(self.state.topleft)
+
+    def update(self, target):
+        self.state = self.camera_func(self.state, target.rect)
+
+def camera_configure(camera, target_rect):
+    l, t, _, _ = target_rect
+    _, _, w, h = camera
+    l, t = -l+WIN_WIDTH / 2, -t+WIN_HEIGHT / 2
+
+    l = min(0, l)                           # Не движемся дальше левой границы
+    l = max(-(camera.width-WIN_WIDTH), l)   # Не движемся дальше правой границы
+    t = max(-(camera.height-WIN_HEIGHT), t) # Не движемся дальше нижней границы
+    t = min(0, t)                           # Не движемся дальше верхней границы
+
+    return Rect(l, t, w, h)
 
 def main():
     pygame.init()
@@ -42,37 +73,75 @@ def main():
     bg = Surface(DISPLAY)
     bg.fill(Color(BACKGROUND_COLOR))
 
-    x=y=0
+    hero = Player(5*32, 5*32)
+
+    timer = pygame.time.Clock()
+
+    #Все живые объекты будут тут
+    entities = pygame.sprite.Group()
+
+    #Тут платформы для проверки пересечения
+    platforms = []
+
+    entities.add(hero)
+
+    x = y = 0
+
     for row in level:
         for col in row:
             if col == "-":
-                pf = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-                pf.fill(Color(PLATFORM_COLOR))
-                screen.blit(pf, (x, y))
+                pf = Platform(x, y)
+                entities.add(pf)
+                platforms.append(pf)
             x += PLATFORM_WIDTH
         y += PLATFORM_HEIGHT
         x = 0
 
+    total_level_width  = len(level[0])*PLATFORM_WIDTH # Высчитываем фактическую ширину уровня
+    total_level_height = len(level)*PLATFORM_HEIGHT   # высоту
+
+    camera = Camera(camera_configure, total_level_width, total_level_height)
+
     while 1:
+        timer.tick(60)
+        left = right = up = down = False
+        playerTurn = False
+
         for e in pygame.event.get():
             if e.type == QUIT:
                 raise SystemExit, "QUIT"
+
+            if e.type == KEYDOWN and e.key == K_LEFT:
+                left = True
+                playerTurn = True
+            if e.type == KEYDOWN and e.key == K_RIGHT:
+                right = True
+                playerTurn = True
+            if e.type == KEYDOWN and e.key == K_DOWN:
+                down = True
+                playerTurn = True
+            if e.type == KEYDOWN and e.key == K_UP:
+                up = True
+                playerTurn = True
+
+
         screen.blit(bg, (0, 0))
 
-        x = y = 0
-        for row in level:
-            for col in row:
-                if col == "-":
-                    pf = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-                    pf.fill(Color(PLATFORM_COLOR))
-                    screen.blit(pf, (x, y))
-                x += PLATFORM_WIDTH
-            y += PLATFORM_HEIGHT
-            x = 0
+        hero.update(left, right, up, down, platforms)
+
+        camera.update(hero)
+        for e in entities:
+            screen.blit(e.image, camera.apply(e))
 
         pygame.display.update()
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
 
 
